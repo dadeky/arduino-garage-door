2d32a0596789f82b7978edcc8c2c73b974b6279d
dvorski <dadeky@gmail.com>
2018-07-12 08:43:23 +0200

    Photon

--- GarageDoor/spec.d
+++ GarageDoor/spec.d
@@ -1,2 +1,2 @@
 spec.o: \
- D:/workspaces/arduino_workspace_oxy/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.c
+ D:/workspaces/arduino_workspace_photon/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.c
